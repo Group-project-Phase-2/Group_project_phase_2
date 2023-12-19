8f3b0d4d98683e0be38f4b8eff470fdb24758ec6
unknown <black.samurai6@gmail.com>
2023-12-19 22:37:54 +0700

    gropu page

--- client/chat-app/src/App.jsx
+++ client/chat-app/src/App.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./page/home";
+import GroupMessage from "./page/GroupMessage";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home></Home>,
+    },
+    {
+      path: "/Group",
+      element: <GroupMessage></GroupMessage>,
     },
   ]);
   return <RouterProvider router={router} />;
---
+++ client/chat-app/src/page/GroupMessage.jsx
@@ -0,0 +1,10 @@
+export default function GroupMessage() {
+  return (
+    <>
+      <div className="container d-flex mt-5">
+        <h1>stes</h1>
+        <h1>tes2</h1>
+      </div>
+    </>
+  );
+}
