71a1a0e09089f5a7b8a839db52a5ff887c71b85b
heru <jrs.herupranata@gmail.com>
2023-12-20 09:10:31 +0700

    - setup message from other user

--- client/chat-app/src/components/Message.jsx
+++ client/chat-app/src/components/Message.jsx
@@ -1,16 +1,31 @@
 const Message = () => {
   return (
-    <div className="message owner">
-      <div className="messageInfo">
-        <img src="" alt="" />
-        <span>just now</span>
+    <>
+      {/* pesan dari user aktif */}
+      <div className="message owner">
+        <div className="messageInfo">
+          <img src="" alt="" />
+          <span>just now</span>
+        </div>
+        <div className="messageContent">
+          <p>isi pesannya</p>
+          {/* jika ada gambar pada pesan */}
+          {/* <img src="" alt="" /> */}
+        </div>
       </div>
-      <div className="messageContent">
-        <p>isi pesannya</p>
-        {/* jika ada gambar pada pesan */}
-        {/* <img src="" alt="" /> */}
+      {/* pesan dari user lain */}
+      <div className="message">
+        <div className="messageInfo">
+          <img src="" alt="" />
+          <span>just now</span>
+        </div>
+        <div className="messageContent">
+          <p>isi pesannya</p>
+          {/* jika ada gambar pada pesan */}
+          {/* <img src="" alt="" /> */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
